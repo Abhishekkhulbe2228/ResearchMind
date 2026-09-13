15f20baedf6630d9cff3cb9863aa601aa78240e8
Abhishek Khulbe <abhishekkhulbe2228@gmail.com>
2026-09-13 18:10:58 +0530

    fixes some bugs

--- agents.py
+++ agents.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 #model setup 
 llm = ChatGroq(
-    model="openai/gpt-oss-120b",
+    model="openai/gpt-oss-20b",
     temperature=0.3,
     groq_api_key=st.secrets["GROQ_API_KEY"]
 )
